4ba91549c1331b7c731f271b65bf83e1d8d6e368
s-prakruthi <prakruthis2004@gmail.com>
2026-02-05 10:31:59 +0530

    Create write_to_git_file.py

---
+++ write_to_git_file.py
@@ -0,0 +1,6 @@
+with open("git_notes.txt", "w") as file:
+    file.write("Hello GitHub!\n")
+    file.write("This file was created using Python.\n")
+    file.write("Learning Git step by step.\n")
+
+print("git_notes.txt created successfully!")
